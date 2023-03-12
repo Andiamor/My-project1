2eae5868989a389f8683a8a4e5c2967c21833baa
Andiamor <andimarmyx@gmail.com>
2023-03-12 16:35:14 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// let str = "Hello.Hello.Hello?Hello.Hello?Hello.Hello.Hello.Hello?Hello.Hello. ";
+// let result = str.match(/?/);
+// console.log(111);
+// let string = "Пре! Пht! dkdjhgkjfhgkdj." 
+
+// function searchNumb (sim, str, numb = 0, arr = []) {
+
+//    let n = str.indexOf(sim, numb+1);
+   
+//    if (n != -1) {
+//        arr[arr.length] = n;
+       
+//        searchNumb (sim, str, n, arr); 
+//    }
+//    return arr;
+// }
+
+// searchNumb ('!', string);
